8332918970cbe7b5d7e332e58f2bf0b818934d0f
ZFusion <1079639527@qq.com>
2023-09-26 14:34:13 +0800

    移除无用资源

--- FlowBusLib/build.gradle.kts
+++ FlowBusLib/build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
 }
 apply("publish.gradle")
 
-group = "com.github.zfusion"
-
 var publishLocal = true
 
 android {
@@ -47,12 +45,12 @@ android {
 dependencies {
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//    implementation("androidx.core:core-ktx:1.9.0")
+//    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation("com.google.android.material:material:1.8.0")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 
